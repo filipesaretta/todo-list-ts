56da13263ea56dc6b32b949a42d54cded4793d93
filipesaretta <filipessm124@gmail.com>
2022-06-15 21:28:22 -0300

    Task can be added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,39 @@
 import { PlusCircle } from 'phosphor-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import logo from '../../assets/logo.svg'
+import { TaskList } from '../TaskList';
 import style from './styles.module.scss'
 
 export function Header() {
+  const [addTask, setAddTask] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  function handleAddTask(e: FormEvent) {
+    e.preventDefault();
+    if (newTask) {
+      setAddTask([...addTask, newTask])
+    }
+    setNewTask('');
+  }
+
+  function handleNewTask(e: ChangeEvent<HTMLInputElement>) {
+    setNewTask(e.target.value);
+  }
+
+
+
   return (
-    <div className={style.container}>
-      <header className={style.header}>
-        <img src={logo} alt="Todo with a rocket" />
-      </header>
-      <form className={style.form}>
-        <input type="text" name="" id="" placeholder='Add your todo, and do it!' />
-        <button type="submit">Create <PlusCircle size={20} weight='bold' /> </button>
-      </form>
-    </div>
+    <>
+      <div className={style.container}>
+        <header className={style.header}>
+          <img src={logo} alt="Todo with a rocket" />
+        </header>
+        <form className={style.form} onSubmit={handleAddTask}>
+          <input type="text" value={newTask} onChange={handleNewTask} placeholder='Add your todo, and do it!' />
+          <button disabled={newTask ? false : true} type="submit">Create <PlusCircle size={20} weight='bold' /></button>
+        </form>
+      </div>
+      <TaskList content={addTask} />
+    </>
   )
 }
